7bb7485535852eb1c065386721f8abafb6948bf8
flip <vlad.flip.prg@gmail.com>
2018-02-13 00:04:18 -0700

    replace tabs with spaces

--- mario_full_pyramid.c
+++ mario_full_pyramid.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 
 int main(void) {
-	int n;
-	    
-	char hash = '#';
-	char space = ' ';
-		  
-	do {
-		scanf("%d", &n);
-	}
-	while (n < 0 || n > 23);
-		    
-	for (int i = 0; i < n; i++) {
-		// print left pyramid
-		for (int j = 0; j < n; j++) {
-			if (j < n - i - 1) {
-				printf("%c", space);
-			} else if (j >= n - i - 1) {
-				printf("%c", hash);
-			}
-		}
+    int n;
+        
+    char hash = '#';
+    char space = ' ';
+          
+    do {
+        scanf("%d", &n);
+    }
+    while (n < 0 || n > 23);
+            
+    for (int i = 0; i < n; i++) {
+        // print left pyramid
+        for (int j = 0; j < n; j++) {
+            if (j < n - i - 1) {
+                printf("%c", space);
+            } else if (j >= n - i - 1) {
+                printf("%c", hash);
+            }
+        }
 
-		//print gap
-		printf("  ");
-				        
-		// print right pyramid
-		for (int j = 0; j < n; j++) {
-			if (j <= i) {
-				printf("%c", hash);
-			} else if (j > i) {
-				printf("%c", space);
-			}
-		}
+        //print gap
+        printf("  ");
+                        
+        // print right pyramid
+        for (int j = 0; j < n; j++) {
+            if (j <= i) {
+                printf("%c", hash);
+            } else if (j > i) {
+                printf("%c", space);
+            }
+        }
 
-		printf("\n");
-					      
-	}
-	return 0;
+        printf("\n");
+                          
+    }
+    return 0;
 }
