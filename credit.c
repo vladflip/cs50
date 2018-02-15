3d8e6d0b06e05f0ea9223d6f51de16098fab3482
flip <vlad.flip.prg@gmail.com>
2018-02-14 22:19:19 -0700

    figure out credit card company

--- credit.c
+++ credit.c
@@ -1,33 +1,68 @@
 #include <stdio.h>
 
 int main(void) {
-    long long card;
+    long long card, CARD;
     int checkSum;
+    int counter = 0;
+    int firstDigit;
 
     do {
         scanf("%lld", &card);
     }
     while (card <= 0);
 
+    CARD = card; 
+    
     while (card != 0) {
-        int odd = card/10%10 * 2;
-        int even = card%10;
+        counter++;
+        int lastDigit = card%10;
 
-        if (odd/10 != 0) {
-            checkSum += odd/10 + odd%10;
-        } else {
-            checkSum += odd;
+        if (lastDigit / 10 == 0) {
+            firstDigit = lastDigit;
         }
 
-        checkSum += even;
+        // odd
+        // even
+        if (counter & 1) {
+            checkSum += lastDigit;
+        } else {
+            if (lastDigit*2 / 10 != 0) {
+                checkSum += lastDigit*2 / 10 + lastDigit*2 % 10;
+            } else {
+                checkSum += lastDigit * 2;
+            }
+        }
 
-        card /= 100;
+        card /= 10;
     }
 
+    int visa, masterCard, amex = 0;
+
     printf("Your checksum is %d. ", checkSum);
+
     if (checkSum % 10 == 0) {
         printf("Congratulations! Your card is valid!\n");
+
+        if (counter == 13) {
+            visa = 1;
+        } else if (counter == 15) {
+            amex = 1;
+        } else if (counter == 16) {
+            if (firstDigit == 4) {
+                visa = 1;
+            } else {
+                masterCard = 1;
+            }
+        }
     } else {
         printf("Sorry, you've entered an invalid card.\n");
+    }
+
+    if (visa) {
+        printf("Your card company is Visa.");
+    } else if (amex) {
+        printf("Your card company is Amex.");
+    } else if (masterCard) {
+        printf("Your card company is MasterCard.");
     }
 }
