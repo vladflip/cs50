93152c132abe39d712f4fc7101d170b64a2c4666
flip <vlad.flip.prg@gmail.com>
2018-02-14 22:58:18 -0700

    rename and style mario

--- mario.c
+++ mario.c
@@ -1,40 +1,52 @@
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void) {
+int main(void)
+{
     int n;
-        
+
     char hash = '#';
     char space = ' ';
-          
-    do {
-        scanf("%d", &n);
+
+    do
+    {
+        n = get_int();
     }
     while (n < 0 || n > 23);
-            
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
+    {
         // print left pyramid
-        for (int j = 0; j < n; j++) {
-            if (j < n - i - 1) {
+        for (int j = 0; j < n; j++)
+        {
+            if (j < n - i - 1)
+            {
                 printf("%c", space);
-            } else if (j >= n - i - 1) {
+            }
+            else if (j >= n - i - 1)
+            {
                 printf("%c", hash);
             }
         }
 
         //print gap
         printf("  ");
-                        
+
         // print right pyramid
-        for (int j = 0; j < n; j++) {
-            if (j <= i) {
+        for (int j = 0; j < n; j++)
+        {
+            if (j <= i)
+            {
                 printf("%c", hash);
-            } else if (j > i) {
+            }
+            else if (j > i)
+            {
                 printf("%c", space);
             }
         }
 
         printf("\n");
-                          
+
     }
     return 0;
 }
