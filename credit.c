be75de941fa76ef300731a0ae072f588cbe80199
flip <vlad.flip.prg@gmail.com>
2018-02-14 23:27:56 -0700

    update credit according to cs50 and style

--- credit.c
+++ credit.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
+int main(void)
+{
     long long card, CARD;
     int checkSum;
     int counter = 0;
     int firstDigit;
 
-    do {
+    do
+    {
         card = get_long_long("Prompt: ");
     }
     while (card <= 0);
 
-    CARD = card; 
-    
-    while (card != 0) {
-        counter++;
-        int lastDigit = card%10;
+    // cache card
+    CARD = card;
 
-        if (lastDigit / 10 == 0) {
+    while (card != 0)
+    {
+        counter++;
+        int lastDigit = card % 10;
+
+        if (lastDigit / 10 == 0)
+        {
             firstDigit = lastDigit;
         }
 
         // odd
         // even
-        if (counter & 1) {
+        if (counter & 1)
+        {
             checkSum += lastDigit;
-        } else {
-            if (lastDigit*2 / 10 != 0) {
-                checkSum += lastDigit*2 / 10 + lastDigit*2 % 10;
-            } else {
+        }
+        else
+        {
+            if (lastDigit * 2 / 10 != 0)
+            {
+                checkSum += lastDigit * 2 / 10 + lastDigit * 2 % 10;
+            }
+            else
+            {
                 checkSum += lastDigit * 2;
             }
         }
@@ -41,28 +52,44 @@ int main(void) {
     int masterCard = 0;
     int amex = 0;
 
-    if (checkSum % 10 == 0) {
-        if (counter == 13) {
+    if (checkSum % 10 == 0)
+    {
+        if (counter == 13)
+        {
             visa = 1;
-        } else if (counter == 15) {
-            amex = 1;
-        } else if (counter == 16) {
-            if (firstDigit == 4) {
-                visa = 1;
-            } else if (firstDigit == 5) {
-                masterCard = 1;
-            } 
         }
-    } else {
-        printf("INVALID\n");
+        else if (counter == 15)
+        {
+            amex = 1;
+        }
+        else if (counter == 16)
+        {
+            if (firstDigit == 4)
+            {
+                visa = 1;
+            }
+            else if (firstDigit == 5)
+            {
+                masterCard = 1;
+            }
+        }
     }
 
-    if (visa) {
+    if (visa)
+    {
         printf("VISA\n");
-    } else if (amex) {
+    }
+    else if (amex)
+    {
         printf("AMEX\n");
-    } else if (masterCard) {
+    }
+    else if (masterCard)
+    {
         printf("MASTERCARD\n");
+    }
+    else
+    {
+        printf("INVALID\n");
     }
 
     return 0;
