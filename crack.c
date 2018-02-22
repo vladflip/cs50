0c32d06d32fbaf21248704041f9be143ba2b3edf
flip <vlad.flip.prg@gmail.com>
2018-02-22 00:07:25 -0700

    update crack

--- crack.c
+++ crack.c
@@ -3,27 +3,26 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <math.h>
 
 #define UPPERSTART 65;
 #define UPPEREND 90;
 #define LOWERSTART 97;
 #define LOWEREND 122;
 
-// get char starting from 0 from A-Z + a-z array: getChar(27) == b
-char getChar(int i)
+// increment char by one in its alphabetical array
+char incrementChar(char c)
 {
-    if (i >= 0 && i <= 25)
+    if (c == 90)
     {
-        return i + UPPERSTART;
+        return 97;
     }
-    else if (i >= 26 && i <= 51)
-    {
-        return i - 26 + LOWERSTART;
-    }
+    return c + 1;
 }
 
-int main(int argc, string argv[])
+int main(void)
 {
+    /*
     if (argc != 2)
     {
         printf("Usage: ./crack hash");
@@ -32,17 +31,57 @@ int main(int argc, string argv[])
 
     string hash = argv[1];
     char salt[3] = {hash[0], hash[1], '\0'};
+    */
 
-    for (int i = 0; i < 52; i++)
+    int n = 3;
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < n; i++)
     {
-        char password[5];
-        password[0] = getChar(i);
-        password[1] = '\0';
+        char password[n];
+        password[i + 1] = '\0';
 
-        if (!strcmp(crypt(password, salt), hash))
+        // compose a string
+        for (int j = 0; j < i + 1; j++)
         {
-            printf("%s\n", password);
+            password[j] = UPPERSTART;
         }
+
+        printf("%sCOMPOSED\n", password);
+
+        for (int j = 0, c = 0, place = n - 1; j < /*(int)pow(52, i + 1)*/ 52*52; j++)
+        {
+            password[i] = alphabet[c];
+
+            printf("%s-", password);
+
+
+            // alphabetical permutation
+            if (c == 51)
+            {
+                c = 0;
+                // use the place value
+                password[i - 1] = incrementChar(password[i - 1]);
+                printf("\n");
+                
+                // move the place value to the left
+                // it should check if its a last letter, if yes
+                // then reset it to a first one, repeat up to the first place
+            }
+            else
+            {
+                c++;
+            }
+
+            /*
+            if (!strcmp(crypt(password, salt), hash))
+            {
+                printf("%s\n", password);
+            }
+            */
+        }
+
+        printf("\n");
     }
 
     return 0;
