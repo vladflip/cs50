52f624f85c3b04a4729f55e71574753fc78554da
vladflip <vlad.flip.prg@gmail.com>
2018-03-01 00:02:43 -0700

    update music

--- music.c
+++ music.c
@@ -7,14 +7,14 @@ int duration(string fraction)
 {
     float numerator = fraction[0] - '0';
     float denumerator = fraction[2] - '0';
-    int result = numerator/denumerator * 8;
+    int result = numerator / denumerator * 8;
 
     return result;
 }
 
 int count_semitones(char note)
 {
-    switch(note)
+    switch (note)
     {
         case 'B':
             return 2;
@@ -38,30 +38,30 @@ int frequency(string note)
     int flat = note[1] == 'b' ? 1 : 0;
     int octave = note[strlen(note) - 1] - '0';
 
-    int frequency = (int)round(440 * pow(2, octave - 4));
+    float frequency = 440 * pow(2, octave - 4);
     int semitones = count_semitones(note[0]);
     float semitone = pow(2, (float)1 / (float)12);
 
     if (letter != 'A')
     {
-        frequency = (int)round((float)frequency * pow((float)2, (float)semitones/(float)12));
+        frequency = (float)frequency * pow((float)2, (float)semitones / (float)12);
     }
 
     if (sharp)
     {
-        frequency = (int)round((float)frequency * semitone);
+        frequency = (float)frequency * semitone;
     }
-    else if(flat)
+    else if (flat)
     {
-        frequency = (int)round((float)frequency / semitone);
+        frequency = (float)frequency / semitone;
     }
 
-    return frequency;
+    return (int)round(frequency);
 }
 
 int main(void)
 {
-    printf("%i\n", frequency("D#7"));
+    printf("%i\n", frequency("Ab3"));
     
     return 0;
 }
